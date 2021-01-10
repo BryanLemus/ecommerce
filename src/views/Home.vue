0465e2119bec168d7637618388fe1f24f5adf52f
Bryan Lemus <brian.lemus@outlook.com>
2021-01-10 15:23:35 -0600

    text classes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="home">
     <navbar placeholder="¿Qué puedo encontrar para ti?"/>
-    <div class="bg-amber-50" style="width: 100px; height: 100px">50</div>
-    <div class="bg-amber-100" style="width: 100px; height: 100px">100</div>
-    <div class="bg-amber-200" style="width: 100px; height: 100px">200</div>
-    <div class="bg-amber-300" style="width: 100px; height: 100px">300</div>
-    <div class="bg-amber-400" style="width: 100px; height: 100px">400</div>
-    <div class="bg-amber-500" style="width: 100px; height: 100px">500</div>
-    <div class="bg-amber-600" style="width: 100px; height: 100px">600</div>
-    <div class="bg-amber-700" style="width: 100px; height: 100px">700</div>
-    <div class="bg-amber-800" style="width: 100px; height: 100px">800</div>
-    <div class="bg-amber-900" style="width: 100px; height: 100px">900</div>
+    <div class="bg-deep-purple-50 text-red-400" style="width: 100px; height: 100px">50</div>
+    <div class="bg-deep-purple-100" style="width: 100px; height: 100px">100</div>
+    <div class="bg-deep-purple-200" style="width: 100px; height: 100px">200</div>
+    <div class="bg-deep-purple-300" style="width: 100px; height: 100px">300</div>
+    <div class="bg-deep-purple-400" style="width: 100px; height: 100px">400</div>
+    <div class="bg-deep-purple-500" style="width: 100px; height: 100px">500</div>
+    <div class="bg-deep-purple-600" style="width: 100px; height: 100px">600</div>
+    <div class="bg-deep-purple-700" style="width: 100px; height: 100px">700</div>
+    <div class="bg-deep-purple-800" style="width: 100px; height: 100px">800</div>
+    <div class="bg-deep-purple-900" style="width: 100px; height: 100px">900</div>
   </div>
 </template>
 
